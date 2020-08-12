8397865e51467cee4072a428972c40be2b5df9bc
Yuriy Sountsov <yuriys@kentico.com>
2020-08-12 13:27:03 -0400

    Begin ZIP boilerplate install

--- App.Infrastructure.Services.BuildService/Properties/AssemblyInfo.cs
+++ App.Infrastructure.Services.BuildService/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("BuildService")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6e4456bc-f293-459e-9412-c4d9d62fdb3f")]
-[assembly: AssemblyVersion("0.1.2")]
-[assembly: AssemblyFileVersion("0.1.2")]
+[assembly: AssemblyVersion("0.1.3")]
+[assembly: AssemblyFileVersion("0.1.3")]
